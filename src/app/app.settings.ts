026b5c8710c7a67f737394c69dd8b2cfbb1017a2
Patrick Okraku <p.okraku@babiel.com>
2021-03-10 21:05:35 +0100

    HTTPS

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
   public static CLIS_URL = 'https://clis.vague.ovh/';
-  public static INFOBOT_API_URL = 'http://tools-bot.ircnet.info/api';
+  public static INFOBOT_API_URL = 'https://tools-bot.ircnet.info/api';
 }
